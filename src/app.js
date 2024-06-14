733311e574ca62b1b9c106589367597956d925b2
ugonna-onyejekwe <innocentugonna063@gmail.com>
2024-06-14 12:09:32 +0100

    edited app.js

--- src/app.js
+++ src/app.js
@@ -13,9 +13,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "https://kaluxia-fe.vercel.app/" }));
+app.use(cors());
+// app.use(cors({ credentials: true, origin: "https://kaluxia-fe.vercel.app/" }));
 app.use(upload());
 app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.get("/", (req, res) => {
+  res.send("kaluxia api");
+});
 
 app.use("/api/users", usersRoute);
 app.use("/api/posts", postsRoute);
@@ -24,5 +29,9 @@ app.use(notFound);
 app.use(errorHandler);
 
 connect(process.env.MONGO_DB_URI)
-  .then(app.listen(PORT, () => console.log(`app runing on port ${PORT}`)))
+  .then(
+    app.listen(PORT, () => {
+      console.log(`app runing on port ${PORT}`);
+    })
+  )
   .catch((err) => console.log(err));
